daadb34182155e3b9b6e95c573fe3e9db2aa1b49
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-06-05 16:04:51 +0300

    fix : examples changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 // import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
-import Layout from "./components/Layout.tsx";
+//import Layout from "./components/Layout.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 // import Counter from "./components/Counter.tsx";
@@ -18,7 +18,12 @@ import Layout from "./components/Layout.tsx";
 import {BrowserRouter, Routes, Route} from "react-router";
 import HomePage from "./pages/HomePage.tsx";
 import NameChangerPage from "./pages/NameChangerPage.tsx";
-
+// import NameChanger from "./components/NameChanger.tsx";
+import OnlineStatusPage from "./pages/OnlineStatusPage.tsx";
+import UserPage from "./pages/UserPage.tsx";
+import RouterLayout from "./components/RouterLayout.tsx";
+import ExamplesPage from "./pages/ExamplesPage.tsx";
+import RouterExamplesLayout from "./components/RouterExamplesLayout";
 
 
 function App() {
@@ -58,12 +63,28 @@ function App() {
             {/*</Layout>*/}
 
                 <BrowserRouter>
-                    <Layout>
-                        <Routes>
-                            <Route path="/" element={<HomePage />}/>
+                    <Routes>
+                        <Route element={<RouterLayout />}>
+                            {/*<Route path="/" element={<HomePage />}/>*/}
+                            <Route index element={<HomePage />}/>
+                            <Route path="users/:userId" element={<UserPage />}/>
+                            <Route path="users" element={<UserPage />}/>
+                        </Route>
+
+                        {/*<Route path="examples?" >*/}
+                        <Route path="examples"  element={<RouterExamplesLayout/>}>
+                            <Route index element={<ExamplesPage/>}/>
                             <Route path="name-changer" element={<NameChangerPage/>}/>
-                        </Routes>
-                    </Layout>
+                            <Route path="online-status" element={<OnlineStatusPage/>}/>
+                        </Route>
+
+                        <Route path="users/:userId" element={<UserPage />}/>
+                        <Route path="users" element={<UserPage />}/>
+                        {/*<Route path="files/*" element={<FilePage/>}/>*/}
+                        PATH: https://example.com/users/125/name/nick
+                        QUERY: https://example.com/users?id=125&name=Nick
+                        https://www.skroutz.gr/c/3074/pagomixanes/f/891854_891908_1066530/trima-epagelmatiki-101-200.html?price_max=3200.0&price_min=1400.001
+                    </Routes>
                 </BrowserRouter>
         </>
     )
